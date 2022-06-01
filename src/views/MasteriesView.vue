cedaa5090a831cde50605170ad47b40b925f98c0
SmashScharrer <SmashScharrer-Dev@outlook.fr>
2022-06-01 17:02:26 +0200

    📦 NEW : Add cards on masteries page

--- src/views/MasteriesView.vue
+++ src/views/MasteriesView.vue
@@ -1,7 +1,49 @@
 <template>
   <div class="masteries">
     <div class="container">
-      <h1 class="h1 text-center">Champions Masteries</h1>
+      <div class="row">
+        <div class="col-12">
+          <h1 class="h1 text-center">Champions Masteries</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <div class="card">
+            <!--            <img src="#" class="card-img-top" alt="IMG Champions" />-->
+            <div class="card-body">
+              <h5 class="card-title text-center">Trundle</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card">
+            <!--            <img src="#" class="card-img-top" alt="IMG Champions" />-->
+            <div class="card-body">
+              <h5 class="card-title text-center">Mordekaiser</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card">
+            <!--            <img src="#" class="card-img-top" alt="IMG Champions" />-->
+            <div class="card-body">
+              <h5 class="card-title text-center">Lux</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
